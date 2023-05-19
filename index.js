72818e2af74864fd3bafb9930c26b654a60dbf4b
Angelica Herrera <angelicaherreraalba@gmail.com>
2023-05-19 15:57:46 -0500

    Modifications for adding month attribute

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true })); //middleware
 
-// send info of the db to the front end
+// read info from the db
+// sends all the table to the front end
 app.get("/api/get", (req, res) => {
   const sqlSelect = "SELECT * FROM expenses;";
   db.query(sqlSelect, (err, result) => {
@@ -29,15 +30,17 @@ app.post("/api/insert", (req, res) => {
   const description = req.body.description;
   const amount = req.body.amount;
   const category = req.body.category;
+  const month = req.body.month;
 
   const sqlInsert =
-    "INSERT INTO expenses (description, amount, category) VALUES (?,?,?);";
+    "INSERT INTO expenses (description, amount, category, month) VALUES (?,?,?,?);";
   // inserting info in our db
-  db.query(sqlInsert, [description, amount, category], (err, result) => {
+  db.query(sqlInsert, [description, amount, category, month], (err, result) => {
     if (err) console.log(err);
   });
 });
 
+//delete smtg from db
 app.delete("/api/delete/:id", (req, res) => {
   const idExpense = req.params.id;
   const sqlDelete = "DELETE FROM expenses WHERE id = (?);";
@@ -45,6 +48,45 @@ app.delete("/api/delete/:id", (req, res) => {
     if (err) console.log(err);
   });
 });
+
+// //updating a expense in the expenses table
+// app.put("/api/updateDB/:id", (req, res) => {
+//   const idPrevious = req.params.id;
+//   const idNew = req.body.id;
+//   const description = req.body.description;
+//   const amount = req.body.amount;
+//   const category = req.body.category;
+
+//   console.log(idPrevious, idNew);
+
+//   const sqlUpdate =
+//     "UPDATE expenses SET id = ?, description = ?, amount = ?, category = ? WHERE id = ?;";
+//   db.query(
+//     sqlUpdate,
+//     [idNew, description, amount, category, idPrevious],
+//     (err, result) => {
+//       if (err) console.log(err);
+//     }
+//   );
+// });
+
+//updating a expense in the expenses table
+// app.put("/api/update/:id", (req, res) => {
+//   const idExpense = req.params.id;
+//   const description = req.body.description;
+//   const amount = req.body.amount;
+//   const category = req.body.category;
+
+//   const sqlUpdate =
+//     "UPDATE expenses SET description = ?, amount = ?, category = ? WHERE id = ?;";
+//   db.query(
+//     sqlUpdate,
+//     [description, amount, category, idExpense],
+//     (err, result) => {
+//       if (err) console.log(err);
+//     }
+//   );
+// });
 
 app.listen(5174, () => {
   console.log("server running on port 5174");
